e564a1c20ab2e1f5f268263fd345e88e9f03928d
jmorenohj <jmorenohj@users.noreply.github.com>
2023-05-24 14:54:51 +0000

    deploy: 9e026d4aac3edce3dd703289fdaef2bb19de9262

--- sketches/convShader.js
+++ sketches/convShader.js
@@ -1,11 +1,11 @@
 let Shader;
 let src;
 let img_src;
-
+let mask;
 
 
 function preload() {
-  Shader = readShader('/showcase/sketches/shaders/conv.frag',
+  Shader = readShader('/showcase/sketches/shaders/fishEye.frag',
     { varyings: Tree.texcoords2 });
 
 
@@ -14,20 +14,26 @@ function preload() {
 }
 
 function setup() {
+  console.log('Hola');
   createCanvas(img_src.height, img_src.width, WEBGL);
   noStroke();
   textureMode(NORMAL);
   shader(Shader);
    
-    src =img_src;
-    
-  
+  src =img_src;
+  //mask = [1, 4, 6, 4, 1,4, 16, 24, 16, 4,6, 24, -476, 24, 6,4, 16, 24, 16, 4,1, 4, 6, 4, 1];
+  //for(let i=0;i<25;i++)mask[i] = -mask[i]/256;
+  //console.log(mask);
+  mask = [-0.125,-0.125,-0.125,-0.125,1,-0.125,-0.125,-0.125,-0.125];
 }
 
 function draw() {
+  
   Shader.setUniform('texture', src);
-  Shader.setUniform('mask',[-0.125,-0.125,-0.125,-0.125,1.0,-0.125,-0.125,-0.125,-0.125]);
+ // Shader.setUniform('mask',mask);fishEye
   Shader.setUniform('texOffset', [1 / src.width, 1 / src.height]);
+  Shader.setUniform('mouse',[mouseX/src.height,mouseY/src.width]);
+  Shader.setUniform('radius',0.2);
   beginShape();
   // format is: vertex(x, y, z, u, v)
   vertex(-1, -1, 0, 0, 1);
